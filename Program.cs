f1abf523b09f8648f08f893870552967dd27cd1e
katesudnik <124512375+katesudnik@users.noreply.github.com>
2023-02-19 22:17:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее. 
+// Console.WriteLine("Input number");
+// var n1 = Decimal.Parse(Console.ReadLine());
+//             var n2 = Decimal.Parse(Console.ReadLine());
+//             if (n1 > n2)
+//                 Console.WriteLine(n1);
+//             else 
+//                 Console.WriteLine(n2);
+
+// // Напишите программу, Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка
+// Console.WriteLine("Введи число");
+// var num = Decimal.Parse(Console.ReadLine());
+// var result = num % 2;
+// Console.WriteLine($"Деление от числа  {num} остаток {result}");
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.WriteLine("Input number");
+// var n1 = Decimal.Parse(Console.ReadLine());
+// var n2 = Decimal.Parse(Console.ReadLine());
+// var n3 = Decimal.Parse(Console.ReadLine());
+//              if (n1 > n2)
+//                  Console.WriteLine(n1);
+//              if (n3 > n1)
+//                  Console.WriteLine(n3);
+//              else 
+//                  Console.WriteLine(n3);
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+// Console.WriteLine("Введите длину: ");
+// int n = int.Parse(Console.ReadLine());
+    
+// for (int i = 1; i <= n; i++)
+//     if (i % 2 == 0)
+//         Console.WriteLine(i);
